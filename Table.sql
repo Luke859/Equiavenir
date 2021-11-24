8e559392de411130de91d1a56e5ab83f2a1323ed
t.bourmaud <tao.bourmaud@ynov.com>
2021-11-24 13:57:21 +0100

    the sauce

---
+++ Table.sql
@@ -0,0 +1,48 @@
+CREATE TABLE User
+(
+	UserId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	UserLastName VARCHAR(80),
+	UserFirstName VARCHAR(80),
+	UserEmail VARCHAR(255),
+	UserLocation VARCHAR(80),
+	UserGender VARCHAR(80),
+	UserPhone VARCHAR(16),
+	UserBirthDate Date
+);
+
+CREATE TABLE Seller
+(
+	SellerId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	UserId INT,
+	CONSTRAINT fk_Seller_User FOREIGN KEY (UserId) REFERENCES User(UserId)
+);
+
+CREATE TABLE Buyer
+(
+	BuyerId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	BuyerRIB VARCHAR(80),
+-- 	BuyerCV VARBINARY(MAX),
+	UserId INT,
+	CONSTRAINT fk_Buyer_User FOREIGN KEY (UserId) REFERENCES User(UserId)
+);
+
+CREATE TABLE Job
+(
+	JobId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	JobName VARCHAR(80),
+	JobDate DATE,
+	JobDuration DATE,
+	JobRemuneration INT,
+	JobDescription VARCHAR(255),
+	SellerId INT,
+	CONSTRAINT fk_Job_Seller FOREIGN KEY (SellerId) REFERENCES Seller(SellerId)
+);
+
+CREATE TABLE Booking
+(
+	BookingId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	BuyerId INT,
+	JobId INT,
+	CONSTRAINT fk_Booking_Job FOREIGN KEY (JobId) REFERENCES Job(JobId),
+	CONSTRAINT fk_Booking_Buyer FOREIGN KEY (BuyerId) REFERENCES Buyer(BuyerId)
+)
