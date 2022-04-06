e4573aaff42d51f305958ad4c75334a0149fac46
t.bourmaud <tao.bourmaud@ynov.com>
2022-04-06 08:22:07 +0200

    .

--- inscription.php
+++ inscription.php
@@ -4,7 +4,52 @@
 <?php ob_start(); ?>
 
 <div id="body">
+
+    <H1>Créez votre compte</H1>
+
     <div id="inscription">
+        <h1>JE CRÉE MON ESPACE CANDIDAT</h1>
+        <div class="CV">
+            <h2>Mon CV</h2>
+            <div class="dragNdrop">
+                <h3>Glissez ou cliquez pour ajouter un CV</h3>
+                <p>Sont acceptés les CV maximum au format PDF, JPEG, PNG, Word</p>
+            </div>
+        </div>
+
+        <form method="POST" action="inscription.php">
+            <div class="infos">
+                <h2>Mes informations</h2>
+                <div class="inputs">
+                    <div class="radioInput">
+                        <label>Civilité*</label>
+                        <input type="radio" id="Madame" name="civilite"><label for="Madame" >Madame</label>
+                        <input type="radio" id="Monsieur" name="civilite" ><label for="Monsieur" >Monsieur</label>
+                    </div>
+                    <span><label>Nom*</label><input type="text"></span>
+                    <span><label>Prénom*</label><input type="text"></span>
+                    <span><label>Email*</label><input type="mail"></span>
+                    <span><label>Mobile</label><input type="text"></span>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- <div id="inscription">
         <h1 class="title">INSCRIPTION</h1>
         <form method="POST" action="inscription.php" id="form">
         <div class="form">
@@ -23,7 +68,7 @@
             <a href="/connexion" name="to-connexion" class="to-connexion">Se connecter</a>
         </div>
         
-    </div>
+    </div> -->
 </div>
 <?php $content = ob_get_clean(); ?> <!-- Fin du template -->
 
@@ -31,24 +76,24 @@
 
 
 <?php
-    if(isset($_POST['btn-inscription'])){
-        $email = $_POST['mail'];
-        $pwd = $_POST['pwd'];
-        $pwdVerif = $_POST['pwd2'];
+    // if(isset($_POST['btn-inscription'])){
+    //     $email = $_POST['mail'];
+    //     $pwd = $_POST['pwd'];
+    //     $pwdVerif = $_POST['pwd2'];
 
-        if($pwd == $pwdVerif){
-            $pwd = password_hash($pwd, PASSWORD_BCRYPT);
-            try{
-                $result = $conn->prepare("INSERT INTO user (UserEmail, UserPwd) VALUES (?, ?);");
-                $result->execute(array($email, $pwd));
-            }catch(PDOException $e){
-                echo $e;
-            }
-            header('Location:http://equiavenir/connexion.php');
-        } else {
-            echo '<script>alert("Confirmation du mot de passe incorrect")</script>';
-        }
+    //     if($pwd == $pwdVerif){
+    //         $pwd = password_hash($pwd, PASSWORD_BCRYPT);
+    //         try{
+    //             $result = $conn->prepare("INSERT INTO user (UserEmail, UserPwd) VALUES (?, ?);");
+    //             $result->execute(array($email, $pwd));
+    //         }catch(PDOException $e){
+    //             echo $e;
+    //         }
+    //         header('Location:http://equiavenir/connexion.php');
+    //     } else {
+    //         echo '<script>alert("Confirmation du mot de passe incorrect")</script>';
+    //     }
 
-    }
+    // }
 
 ?>
